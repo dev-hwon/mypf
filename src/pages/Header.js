85ac02179978d9577956e20233dcf269faa6406a
dev-hwon <thehyun1@nate.com>
2023-01-31 21:39:58 +0900

    경로변경

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,48 +1,80 @@
-import '../Header.css';
-import img_im from '../images/imgMe.jpg'
-import { Link, useParams } from 'react-router-dom';
-import { useState } from 'react';
+//eslint-disable
+import "../Header.css";
+import img_im from "../images/imgMe.jpg";
+import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
 
 export default function Header(props) {
-    const params = useParams();
-    let themeClick = (e) => {
-        props.theme(e.target.dataset.color)
-    }
+  const params = useParams();
+  let themeClick = (e) => {
+    props.theme(e.target.dataset.color);
+  };
 
-    // 루트컬러변경
-    // document.documentElement.style.setProperty('--your-variable', '#YOURCOLOR');
-    return (
-        <div id="header">
-            
-            <div className="my_info">
-                <div className="img_im"><img src={img_im} alt="" /></div>
-                <div className="tx_id">DEV_HWON</div>
-                <div className="tx_summary">8년차 UI개발자 현원입니다.<br />반갑습니다. </div>
-                <div className="tx_link tx_shortening_1">GIT : <Link to="https://github.com/dev-hwon/" target="_brank">https://github.com/dev-hwon/</Link></div>
-                <div className="tx_link tx_shortening_1">BEFORE PF : <Link to="http://thehyun1.dothome.co.kr/" target="_brank">http://thehyun1.dothome.co.kr/</Link></div>
-                <div className="tx_callnumber">Phon : 010-8738-7473</div>
-                <div className="tx_mail">Mail : <a href="mailto:thehyun11@google.com">thehyun11@google.com</a></div>
-            </div>
-            <nav className="my_gnb">
-                <ul>
-                    <li><Link to="/">Main</Link></li>
-                    <li><Link to="/Pflist/enuri">enuri.com</Link></li>
-                    <li><Link to="/Pflist/fuz">fuz</Link></li>
-                    <li><Link to="/Pflist/off">off</Link></li>
-                    <li><Link to="/Pflist/vida">visual dive</Link></li>
-                </ul>
-            </nav>
-            <div className='my_theme'>
-                <div className='tx_tit'>COLOR THEME</div>
-                <ul>
-                    <li onClick={themeClick} data-color="dark"></li>
-                    <li onClick={themeClick} data-color="white"></li>
-                    <li onClick={themeClick} data-color="blue"></li>
-                </ul>
-            </div>
-            <div className='my_copyright'>
-                thehyun1.design ⓒ All right reserved.
-            </div>
+  // 루트컬러변경
+  // document.documentElement.style.setProperty('--your-variable', '#YOURCOLOR');
+  return (
+    <div id="header">
+      <div className="my_info">
+        <div className="img_im">
+          <img src={img_im} alt="" />
         </div>
-    );
+        <div className="tx_id">DEV_HWON</div>
+        <div className="tx_summary">
+          8년차 UI개발자 현원입니다.
+          <br />
+          반갑습니다.
+        </div>
+        <div className="tx_link tx_shortening_1">
+          GIT :
+          <Link to="https://github.com/dev-hwon/" target="_brank">
+            https://github.com/dev-hwon/
+          </Link>
+        </div>
+        <div className="tx_link tx_shortening_1">
+          BEFORE PF :
+          <Link to="http://thehyun1.dothome.co.kr/" target="_brank">
+            http://thehyun1.dothome.co.kr/
+          </Link>
+        </div>
+        <div className="tx_callnumber">Phon : 010-8738-7473</div>
+        <div className="tx_mail">
+          Mail : <a href="mailto:thehyun11@google.com">thehyun11@google.com</a>
+        </div>
+      </div>
+      <nav className="my_gnb">
+        <ul>
+          <li>
+            <Link to="/">Main</Link>
+          </li>
+          <li>
+            <Link to="/Pflist/enuri">enuri.com</Link>
+          </li>
+          <li>
+            <Link to="/Pflist/fuz">fuz</Link>
+          </li>
+          <li>
+            <Link to="/Pflist/off">off</Link>
+          </li>
+          <li>
+            <Link to="/Pflist/vida">visual dive</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="my_theme">
+        <div className="tx_tit">COLOR THEME</div>
+        <ul>
+          <li>
+            <button onClick={themeClick} data-color="dark"></button>
+          </li>
+          <li>
+            <button onClick={themeClick} data-color="white"></button>
+          </li>
+          <li>
+            <button onClick={themeClick} data-color="blue"></button>
+          </li>
+        </ul>
+      </div>
+      <div className="my_copyright">thehyun1.design ⓒ All right reserved.</div>
+    </div>
+  );
 }
--- src/pages/Pflist.js
+++ src/pages/Pflist.js
@@ -1,67 +1,82 @@
-import '../Pflist.css';
-import { useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import pflist from '../db/pf_list.json'
-import Modal from '../component/Modal';
-import useAsync from '../hooks/useAsync';
+import "../Pflist.css";
+import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import pflist from "../db/pf_list.json";
+import Modal from "../component/Modal";
+import useAsync from "../hooks/useAsync";
 // import axios from 'axios';
 
 function getLists(params) {
-    return pflist.pflist[0][params.name];
+  return pflist.pflist[0][params.name];
 }
-function viewDetailImg() {
-
-}
+function viewDetailImg() {}
 const Pflist = () => {
-    const [modalVisible, setModalVisible] = useState(false)
-    const [imgUrl, setImgUrl] = useState('');
-    const openModal = (e) => {
-        setModalVisible(true)
-        console.log(e.target.value);
-        setImgUrl(e.target.value);
-    }
-    const closeModal = () => {
-        setModalVisible(false)
-    }
-    const params = useParams();
-    const [state] = useAsync(() => getLists(params), [params], true);
-    const { loading, data: lists, error } = state;
-    if (loading) return <div>로딩중..</div>;
-    if (error) return <div>에러가 발생했습니다</div>;
-    if (!lists) return null;
+  const [modalVisible, setModalVisible] = useState(false);
+  const [imgUrl, setImgUrl] = useState("");
+  const openModal = (e) => {
+    setModalVisible(true);
+    console.log(e.target.value);
+    setImgUrl(e.target.value);
+  };
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+  const params = useParams();
+  const [state] = useAsync(() => getLists(params), [params], true);
+  const { loading, data: lists, error } = state;
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!lists) return null;
 
-
-    return (
-        <div className="pflist">
-            <ul className="col_wrap col_gap_16">
-                {lists.map((list, index) => (
-                    <li
-                        className="col col_xs_12 col_sm_4 col_md_3 col_lg_2"
-                        key={index}
-                    >
-                        <div className="list_item">
-                            <div className="item__label">{list.label}</div>
-                            <div className="item__title">{list.title}</div>
-                            <div className="item__summary tx_shortening_2">{list.summary}</div>
-                            <div className="item__date">{list.date}</div>
-                            <div className="item__image"><img src={`${process.env.PUBLIC_URL}` + list.img_url} alt="" /></div>
-                            <div className="item__cover">
-                                {!list.link ?
-                                    <button onClick={openModal} className='common_btn btn_sm btn_default bt__detail_view' value={list.detailImg_url}>자세히보기</button> :
-                                    <a href={list.url} target="_blank" className="common_btn btn_sm btn_default bt__detail_page">이동</a>
-                                }
-                            </div>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-            {modalVisible && <Modal
-                visible={modalVisible}
-                closable={true}
-                maskClosable={true}
-                onClose={closeModal}><img src={imgUrl} alt="" /></Modal>}
-
-        </div>
-    )
-}
+  return (
+    <div className="pflist">
+      <ul className="col_wrap col_gap_16">
+        {lists.map((list, index) => (
+          <li className="col col_xs_12 col_sm_4 col_md_3 col_lg_2" key={index}>
+            <div className="list_item">
+              <div className="item__label">{list.label}</div>
+              <div className="item__title">{list.title}</div>
+              <div className="item__summary tx_shortening_2">
+                {list.summary}
+              </div>
+              <div className="item__date">{list.date}</div>
+              <div className="item__image">
+                <img src={list.img_url} alt="" />
+              </div>
+              <div className="item__cover">
+                {!list.link ? (
+                  <button
+                    onClick={openModal}
+                    className="common_btn btn_sm btn_default bt__detail_view"
+                    value={list.detailImg_url}
+                  >
+                    자세히보기
+                  </button>
+                ) : (
+                  <a
+                    href={list.url}
+                    target="_blank"
+                    className="common_btn btn_sm btn_default bt__detail_page"
+                  >
+                    이동
+                  </a>
+                )}
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+      {modalVisible && (
+        <Modal
+          visible={modalVisible}
+          closable={true}
+          maskClosable={true}
+          onClose={closeModal}
+        >
+          <img src={imgUrl} alt="" />
+        </Modal>
+      )}
+    </div>
+  );
+};
 export default Pflist;
